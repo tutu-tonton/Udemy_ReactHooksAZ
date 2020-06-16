c99bd87e161066d6df392ac859d8682c839bf00c
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-06-16 19:28:28 +0900

    21.Component Communication.

--- src/App.js
+++ src/App.js
@@ -13,24 +13,36 @@ const initList = [
 const App = () => {
 	const [list, setList] = useState(initList);
 
-	// listからカロリーが50より多いものを削除する
-	const removeUnhealthyHandle = (e) => {
-		// e.preventDefault();
-		const filteredList = list.filter((ele) => ele.calorie < 50);
-
+	// 各アイテムのボタンを押すと、そのアイテムが削除されるようにする
+	// どのアイテムのボタンが押されたかを判別できないといけない
+	// -> 各ボタンのname属性利用
+	// -> e.target.nameで判別可能
+	const removeItemHandle = (e) => {
+		console.dir(e.target.name);
+		// クリックされたアイテムを削除する
+		// => クリックされたアイテム以外で新配列作る
+		const filteredList = list.filter((ele) => ele.name !== e.target.name);
 		setList(filteredList);
 	};
+
+	// // listからカロリーが50より多いものを削除する
+	// const removeUnhealthyHandle = (e) => {
+	// 	// e.preventDefault();
+	// 	const filteredList = list.filter((ele) => ele.calorie < 50);
+	// 	setList(filteredList);
+	// };
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h2>Grocery List</h2>
 				{list.map((ele, idx) => {
-					return <Item key={`${idx}${ele.name}${ele.calorie}`} item={ele}></Item>;
+					// 各ボタンが押された時の処理は、親側で処理する
+					return <Item onClick={removeItemHandle} key={`${idx}${ele.name}${ele.calorie}`} item={ele}></Item>;
 				})}
-				<button onClick={removeUnhealthyHandle} className="remove-button">
+				{/* <button onClick={removeUnhealthyHandle} className="remove-button">
 					Remove Unhealthy
-				</button>
+				</button> */}
 			</header>
 		</div>
 	);
--- src/components/item.component.jsx
+++ src/components/item.component.jsx
@@ -7,6 +7,12 @@ const item = (props) => {
 		<div className="item-style">
 			<h3>{props.item.name}</h3>
 			<h3>{props.item.calorie}</h3>
+			{/* 各ボタンにはname属性が設定される */}
+			{/* -> どのボタンが押されたか判別ができる */}
+			{/* クリックされた時の処理は、親コンポーネント側で処理する */}
+			<button name={props.item.name} className="remove-button" onClick={props.onClick}>
+				Remove
+			</button>
 		</div>
 	);
 };
