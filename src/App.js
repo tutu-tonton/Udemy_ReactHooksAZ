5773357cf9d0ab41e3767debd39899c3204c38df
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-06-16 14:42:32 +0900

    15. useState. 一番基本的な構造

--- src/App.js
+++ src/App.js
@@ -1,58 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import NameTag from '../src/component/nameTag.component';
 import './App.scss';
 
-//========================================
-// 　HOC
-// 　引数を必要として、さらにpropsも必要
-//========================================
-const makeGreen = (BaseComponent) => (props) => {
-	const addGreen = {
-		style: {
-			color: 'green',
-		},
-	};
-
-	const newProps = {
-		...props,
-		...addGreen,
-	};
-
-	return <BaseComponent {...newProps} />;
-};
-
-const GreenNameTag = makeGreen(NameTag);
-
-//
-//========================================
-
-const removeInline = (BaseComponent) => (props) => {
-	const newProps = { ...props };
-	delete newProps.style;
-	return <BaseComponent {...newProps} />;
-};
-
-const CleanNameTag = removeInline(NameTag);
-
-//========================================
-//  JSX
-//========================================
 const App = () => {
+	const [age, setAge] = useState(21);
+
+	const ageUpHandle = () => {
+		setAge(age + 1);
+	};
+	const ageDownHandle = () => {
+		setAge(age - 1);
+	};
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1 className="name title">Names List</h1>
-				<GreenNameTag firstName="peter" lastName="peterson"></GreenNameTag>
-				<CleanNameTag firstName="john" lastName="johnson"></CleanNameTag>
-				<NameTag firstName="jill" lastName="jillson"></NameTag>
-				<NameTag></NameTag>
+				<h1>Use State Hook</h1>
+				<h2>Age: {age}</h2>
+				<button onClick={ageUpHandle}>Age Up</button>
+				<button onClick={ageDownHandle}>Age down</button>
 			</header>
 		</div>
 	);
 };
 
 export default App;
+
+// import NameTag from '../src/component/nameTag.component';
+// //========================================
+// // 　HOC
+// // 　引数を必要として、さらにpropsも必要
+// //========================================
+// const makeGreen = (BaseComponent) => (props) => {
+// 	const addGreen = {
+// 		style: {
+// 			color: 'green',
+// 		},
+// 	};
+
+// 	const newProps = {
+// 		...props,
+// 		...addGreen,
+// 	};
+
+// 	return <BaseComponent {...newProps} />;
+// };
+
+// const GreenNameTag = makeGreen(NameTag);
+
+// //
+// //========================================
+
+// const removeInline = (BaseComponent) => (props) => {
+// 	const newProps = { ...props };
+// 	delete newProps.style;
+// 	return <BaseComponent {...newProps} />;
+// };
+
+// const CleanNameTag = removeInline(NameTag);
+
+// //========================================
+// //  JSX
+// //========================================
+// const App = () => {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<h1 className="name title">Names List</h1>
+// 				<GreenNameTag firstName="peter" lastName="peterson"></GreenNameTag>
+// 				<CleanNameTag firstName="john" lastName="johnson"></CleanNameTag>
+// 				<NameTag firstName="jill" lastName="jillson"></NameTag>
+// 				<NameTag></NameTag>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
+// export default App;
 
 //========================================
 //  分割代入でスタイル上書き
